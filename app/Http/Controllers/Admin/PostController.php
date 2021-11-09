82692051a2626e9c76ddca13048136e33386b567
Hamza <Hamza5955>
2021-11-09 15:30:16 +0100

    modify e delete

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 use App\Post;
 
 class PostController extends Controller
@@ -26,7 +27,7 @@ class PostController extends Controller
      */
     public function create()
     {
-        //
+        return view("admin.posts.create");
     }
 
     /**
@@ -37,7 +38,37 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // Per prima cosa valido i dati che arrivano dal form
+        $request->validate([
+            "title" => "required",
+            "content" => "required"
+        ]);
+
+        $form_data = $request->all();
+
+        $new_post = new Post();
+        $new_post->fill($form_data);
+
+        // Titolo: il mio post
+        // Slug: il-mio-post
+
+        $slug = Str::slug($new_post->title, "-");
+        // $slug_base = $slug;
+
+        $slug_presente = Post::where("slug", $slug)->first();
+
+        $contatore = 1;
+
+        while ($slug_presente) {
+            $slug = $slug . "-" . $contatore;
+            $slug_presente = Post::where("slug", $slug)->first();
+            $contatore++;
+        }
+
+        $new_post->slug = $slug;
+        $new_post->save();
+
+        return redirect()->route("admin.posts.index")->with("inserted", "Il post è stato correttamente salvato");
     }
 
     /**
@@ -48,43 +79,77 @@ class PostController extends Controller
      */
     public function show($id)
     {
-        $post = Post::where('id', $id)->first();
-        if(!$post){
+        $post = Post::where("id", $id)->first();
+        if (!$post) {
             abort(404);
-        }return view('admin.posts.show', compact('post'));
+        }
+        return view("admin.posts.show", compact("post"));
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param  Post $post
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Post $post)
     {
-        //
+        if (!$post) {
+            abort(404);
+        }
+
+        return view("admin.posts.edit", compact("post"));
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  Post $post
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Post $post)
     {
-        //
+        $request->validate([
+            "title" => "required",
+            "content" => "required"
+        ]);
+
+        $form_data = $request->all();
+
+        // Verifico se il titolo ricevuto dal form è diverso dal vecchio titolo
+
+        if ($form_data["title"] != $post->title) {
+            // è stato modificato il titolo, quindi devo modificare anche lo slug
+
+            $slug = Str::slug($form_data["title"], "-");
+
+            $slug_presente = Post::where("slug", $slug)->first();
+
+            $contatore = 1;
+            while ($slug_presente) {
+                $slug = $slug . "-" . $contatore;
+                $slug_presente = Post::where("slug", $slug)->first();
+                $contatore++;
+            }
+
+            $form_data["slug"] = $slug;
+        };
+
+        $post->update($form_data);
+
+        return redirect()->route("admin.posts.index")->with("updated", "Post correttamente aggiornato");
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  Post  $post
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Post $post)
     {
-        //
+        $post->delete();
+        return redirect()->route("admin.posts.index")->with("deleted", "Post eliminato");
     }
 }
