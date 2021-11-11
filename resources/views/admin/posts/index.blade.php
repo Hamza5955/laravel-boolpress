162fc0db1e4562a5311e17c50e234757053be1ca
Hamza <Hamza5955>
2021-11-11 15:49:37 +0100

    corezione esercizio

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -39,7 +39,7 @@
                <td scope="row"> {{ $post["id"] }}</td>
                <td>{{ $post["title"] }}</td>
                <td>{{ $post["slug"] }}</td>
-               <!-- @dump($post->category()) -->
+                {{-- @dump($post->category()) --}}
                <td>
                   @if ($post["category"])
                       {{ $post["category"]["name"] }}
@@ -62,32 +62,6 @@
                   </form>
                </td> --}}
             </tr>
-
-            <!-- Modal -->
-            <div class="modal fade" id="deletePost{{$post->id}}" tabindex="-1" aria-labelledby="deletePostLabel" aria-hidden="true">
-               <div class="modal-dialog">
-                  <div class="modal-content">
-                     <div class="modal-header">
-                        <h5 class="modal-title" id="deletePostLabel">Delete confirmation: Post # {{$post->id}}</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                           <span aria-hidden="true">&times;</span>
-                        </button>
-                  </div>
-                  <div class="modal-body">
-                     <p>You are about to <strong>permanently</strong> delete the post from the database.</p>
-                     <p>Are you sure you want to delete it?</p>
-                     <p><strong>The action cannot be reversed</strong></p>
-                  </div>
-                  <div class="modal-footer">
-                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                     <form action="{{route('admin.posts.destroy', $post->id)}}" method="POST">
-                         @csrf
-                         @method('DELETE')
-                         <button type="submit" class="btn btn-danger">Delete</button>
-                     </form>
-                  </div>
-               </div>
-            </div>
          @endforeach
       </tbody>
    </table>
